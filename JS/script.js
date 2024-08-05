c232f0acc7d1568015594423223209daa399bcc0
Andrea Ortiz Alvarado <andreaoal98@gmail.com>
2024-08-05 14:32:23 -0600

    EJERCICIOS DE FRONTEND

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+console.log("Hola mundo soy yo ");
+
+// Este es un comentario 
+/*
+Este es un comentario de bloque
+*/
+
+/*
+Paréntesis: ()
+Exponenciación: **
+Multiplicación: *, División: /, Módulo: %
+Suma: +, Resta: -
+Operadores relacionales: <, <=, >, >=
+Igualdad: ==, ===, !=, !==
+Operadores lógicos: &&, ||
+Operador ternario: ? :
+Asignación: =, +=, -=, *=, /=, %=, **=, etc
+*/ 
+
+//Utiliza la función de alerta de JavaScript para mostrar mensajes. 
+alert('ALERTAAA');
+
+//Utiliza la función JavaScript console.log para registrar mensajes
+console.log("Este es un mensaje para el ejercicio");
+
+//invocar funciones definidas JavaScript mediante la propiedad onclick de las etiquetas HTML
+function sorpresa() {alert("Tu puedes, sigue así");}
